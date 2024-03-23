78f9ddfb2c8dd8e4efffa7acf2fd62ad8ce12e53
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-03-23 20:47:04 +0700

    edit config

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.use("/api/project/auth", auth);
 app.use("/api/project/reservations", reservations);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,'on '+ process.env.HOST +' :' + PORT));
+const server = app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,'on '+ process.env.HOST +':' + PORT));
 
 //Handle unhandled promise rejections
 process.on(`unhandledRejection`,(err,promise)=>{
