06d47918b45712143b1ef7acb39500c08454f3b1
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-03-23 21:50:54 +0700

    edit config

--- server.js
+++ server.js
@@ -97,6 +97,7 @@ const reservations = require("./routes/reservations");
 
 const app=express();
 
+const PORT = process.env.PORT || 5000;
 //Body parser
 app.use(express.json());
 
@@ -148,7 +149,7 @@ app.use("/api/project/coworkings", coworkings);
 app.use("/api/project/auth", auth);
 app.use("/api/project/reservations", reservations);
 
-const PORT = process.env.PORT || 5000;
+
 const server = app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,'on '+ process.env.HOST +':' + PORT));
 
 //Handle unhandled promise rejections
